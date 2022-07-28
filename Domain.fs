a91a7901e582d7f2fed609749823345286c722c9
Akash Mair <akash@compositional-it.com>
2022-07-28 20:53:34 +0100

    added domain types

--- Domain.fs
+++ Domain.fs
@@ -1,2 +1,46 @@
 ﻿module Domain
 
+open System
+
+type LocationId = LocationId of Guid 
+type LocationName = LocationName of string
+
+type Location =
+    {
+        Name: LocationName
+        Id: LocationId
+    }
+
+type ReservationId = ReservationId of Guid
+type ReservationStatus =
+    | Requested
+    | Processing
+    | Successful
+    | Failed 
+
+type Reservation =
+    {
+        Id: ReservationId
+        Date: DateOnly
+        Time: TimeOnly
+        Seats: int
+        SpecialRequest: string option
+        Status: ReservationStatus
+        Location: LocationId
+    }
+
+type BookingId = BookingId of Guid
+
+type Booking =
+    {
+        Id: BookingId
+        Date: DateOnly
+        Time: TimeOnly
+        Seats: int
+        SpecialRequest: string option
+        Location: LocationId 
+        Reservation: ReservationId
+    }
+
+
+
